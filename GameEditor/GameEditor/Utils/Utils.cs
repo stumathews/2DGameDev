e530ef6ed17e47141a0f7afa401f7e7fc22d401a
Stuart Mathews <stumathews@gmail.com>
2023-02-18 17:46:43 +0000

    Simplify game editor xml loading and saving

---
+++ GameEditor/GameEditor/Utils/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GameEditor.Utils
+{
+    public static class Utils
+    {
+        public static int ToNumber(this string str) => int.Parse(str);
+        public static string ToBoolString(this Visibility visibility) => visibility == Visibility.Visible ? Boolean.TrueString : Boolean.FalseString;
+        public static Visibility ToVisibility(this string str) => (Visibility)Enum.Parse(typeof(Visibility), str);
+        public static Visibility VisibilityFromTruthString(this string truth) => bool.Parse(truth) ? Visibility.Visible : Visibility.Hidden;
+    }
+}
--- GameEditor/GameEditor/ViewModels/LevelManager.cs
+++ GameEditor/GameEditor/ViewModels/LevelManager.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Xml;
+using System.Xml.Linq;
 using GameEditor.Models;
+using GameEditor.Utils;
 using Microsoft.Win32;
 
 namespace GameEditor.ViewModels
@@ -31,50 +34,38 @@ namespace GameEditor.ViewModels
                 if (!(saveFileDialog.ShowDialog() is true)) return;
 
                 OnAboutToSaveFile?.Invoke(this, $"Saving File '{saveFileDialog.FileName}'...");
+                
+                var levelNode = new XElement("level",
+                    new XAttribute("cols", level.NumCols),
+                    new XAttribute("rows", level.NumRows),
+                    new XAttribute("autoPopulatePickups", level.AutoPopulatePickups.ToString()),
+                    from room in level.Rooms
+                        
+                    let gameObject = room.ResidentGameObjectType
+                    let roomEl = new XElement("room",
+                        new XAttribute("number", room.RoomNumber),
+                        new XAttribute("top", room.TopWallVisibility.ToBoolString()),
+                        new XAttribute("right", room.RightWallVisibility.ToBoolString()),
+                        new XAttribute("bottom", room.BottomWallVisibility.ToBoolString()),
+                        new XAttribute("left", room.LeftWallVisibility.ToBoolString()),
+                        // <object>
+                        gameObject != null 
+                            ? new XElement("object",
+                            new XAttribute("name", gameObject.Name),
+                            new XAttribute("type", gameObject.Type ?? ""),
+                            new XAttribute("resourceId", gameObject.ResourceId),
+                            new XAttribute("assetPath", gameObject.AssetPath), 
+                            // <property>
+                            from property in gameObject.Properties
+                            let key = new XAttribute("name", property.Key)
+                            let value = new XAttribute("value", property.Value)
+                            select new XElement("property", key, value)) 
+                            : null) 
+                    select roomEl);
 
                 using (var writer = XmlWriter.Create(saveFileDialog.FileName, xmlSettings))
                 {
-                    writer.WriteStartDocument();
-                    writer.WriteStartElement("level"); // <level ...
-                    writer.WriteAttributeString("cols", level.NumCols.ToString());
-                    writer.WriteAttributeString("rows", level.NumRows.ToString());
-                    writer.WriteAttributeString("autoPopulatePickups", level.AutoPopulatePickups.ToString());
-                    foreach(var roomViewModel in level.Rooms)
-                    {
-                        var topVisible = roomViewModel.TopWallVisibility == Visibility.Visible;
-                        var rightVisible = roomViewModel.RightWallVisibility == Visibility.Visible;
-                        var bottomVisible = roomViewModel.BottomWallVisibility == Visibility.Visible;
-                        var leftVisible = roomViewModel.LeftWallVisibility == Visibility.Visible;
-
-                        writer.WriteStartElement("room"); // <room ...
-                        writer.WriteAttributeString("number", roomViewModel.RoomNumber.ToString());
-                        writer.WriteAttributeString("top", topVisible.ToString() );
-                        writer.WriteAttributeString("right", rightVisible.ToString() );
-                        writer.WriteAttributeString("bottom", bottomVisible.ToString() );
-                        writer.WriteAttributeString("left", leftVisible.ToString() );
-
-                        if(roomViewModel.ResidentGameObjectType != null)
-                        {
-                            var gameObjectType = roomViewModel.ResidentGameObjectType;
-                            writer.WriteStartElement("object"); //<object ...
-                            writer.WriteAttributeString("name", gameObjectType.Name);
-                            writer.WriteAttributeString("type", gameObjectType.Type);
-                            writer.WriteAttributeString("resourceId", gameObjectType.ResourceId.ToString());
-                            writer.WriteAttributeString("assetPath", gameObjectType.AssetPath);
-                            foreach(var property in gameObjectType.Properties)
-                            {
-                                writer.WriteStartElement("property"); //<property ..
-                                writer.WriteAttributeString("name", property.Key);
-                                writer.WriteAttributeString("value", property.Value);
-                                writer.WriteEndElement();
-                            }
-                            writer.WriteEndElement();
-                        }
-
-                        writer.WriteEndElement();
-                    }
-                    writer.WriteEndElement();
-                    writer.WriteEndDocument();
+                    levelNode.WriteTo(writer);
                 }
 
                 OnFileSaved?.Invoke(this,  $"Saved File '{saveFileDialog.FileName}'.");
@@ -88,86 +79,44 @@ namespace GameEditor.ViewModels
         public Level LoadLevelFile()
         {
             var openFileDialog = new OpenFileDialog();
-            var level = new Level();
 
+            Level level = null;
             if(openFileDialog.ShowDialog() is true)
             {
-                var settings = new XmlReaderSettings
-                {
-                    DtdProcessing = DtdProcessing.Ignore
-                };
-                var reader = XmlReader.Create(openFileDialog.FileName, settings);
-                RoomViewModel roomViewModel = null;
-                
-                while (reader.Read())
-                {                    
-                    if (reader.NodeType == XmlNodeType.Element)
-                    {
-                        if(reader.Name.Equals("level"))
+                level = (from levels in XElement.Load(openFileDialog.FileName).AncestorsAndSelf()
+                        select new Level
                         {
-                            level.NumCols = int.Parse(reader.GetAttribute("cols") ?? throw new Exception(
-                                "NumCols Not found"));
-                            level.NumRows = int.Parse(reader.GetAttribute("rows") ?? throw new Exception(
-                                "NumRows Not found"));
-                            level.AutoPopulatePickups = bool.Parse(reader.GetAttribute("autoPopulatePickups") ?? bool.FalseString);
-                        }
-
-                        roomViewModel = new RoomViewModel();
-
-                        if(reader.Name.Equals("room"))
-                        {
-                            roomViewModel.RoomNumber = int.Parse(reader.GetAttribute("number") ?? "0");
-                            roomViewModel.TopWallVisibility = bool.Parse(reader.GetAttribute("top") ??
-                                                                         throw new Exception(
-                                                                             "Top wall visibility Not found"))
-                                ? Visibility.Visible
-                                : Visibility.Hidden;
-                            roomViewModel.RightWallVisibility = bool.Parse(reader.GetAttribute("right") ??
-                                                                           throw new Exception(
-                                                                               "Right wall visibility Not found"))
-                                    ? Visibility.Visible
-                                    : Visibility.Hidden;
-                            roomViewModel.BottomWallVisibility = bool.Parse(reader.GetAttribute("bottom") ??
-                                                                            throw new Exception(
-                                                                                "Bottom wall visibility Not found"))
-                                    ? Visibility.Visible
-                                    : Visibility.Hidden;
-                            roomViewModel.LeftWallVisibility =
-                                bool.Parse(reader.GetAttribute("left") ??
-                                           throw new Exception("Left wall visibility Not found"))
-                                    ? Visibility.Visible
-                                    : Visibility.Hidden;
-                        }
-                        if (reader.Name.Equals("object"))
-                        {
-                            roomViewModel.ResidentGameObjectType = new GameObjectType
+                            Rooms = levels.Descendants("room").Select(r => new RoomViewModel
                             {
-                                AssetPath = reader.GetAttribute("assetPath"),
-                                Name = reader.GetAttribute("name"),
-                                ResourceId = int.Parse(reader.GetAttribute("resourceId") ?? throw new Exception("Resource Id Not found")),
-                                Type = reader.GetAttribute("type"),
-                                Properties = new List<KeyValuePair<string, string>>()
-                            };
-                        }
+                                RoomNumber = GetAsNumber(r, "number"),
+                                TopWallVisibility = GetAsVisibilityFromTruthString(r,"top"),
+                                LeftWallVisibility = GetAsVisibilityFromTruthString(r,"left"),
+                                RightWallVisibility = GetAsVisibilityFromTruthString(r,"right"),
+                                BottomWallVisibility = GetAsVisibilityFromTruthString(r, "bottom"),
+                                ResidentGameObjectType = r.Descendants("object").Select(o => new GameObjectType()
+                                {
+                                    Name = GetAsString(o, "name"),
+                                    AssetPath = GetAsString(o, "assetPath"),
+                                    Properties = o.Descendants("property").Select(p => new KeyValuePair<string, string>(key: GetAsString(p, "name"), value: GetAsString(p, "value"))).ToList()
+                                }).SingleOrDefault()
+                            }).ToList(),
+                        }).SingleOrDefault();
 
-                        if (reader.Name.Equals("property"))
-                        {
-                            for (var i = 0; i < reader.AttributeCount; i++)
-                            {
-                                reader.MoveToAttribute(i);
-                                roomViewModel.ResidentGameObjectType.Properties.Add(new KeyValuePair<string, string>(reader.Name, reader.Value));
-                            }
-                        }
-                    }
-
-                    if ((reader.NodeType == XmlNodeType.EndElement || reader.IsEmptyElement) && reader.Name.Equals("room"))
-                    {
-                        level.Rooms.Add(roomViewModel);
-                    }
-                }
             }
             OnLevelLoaded?.Invoke(this, EventArgs.Empty);
             return level;            
         }
+
+        private static string GetAsString(XElement o, string attributeName) 
+            => (o.Attribute(attributeName) ?? throw new NullReferenceException(attributeName)).Value;
+
+        private static Visibility GetAsVisibility(XElement r, string attributeName) 
+            => GetAsString(r, attributeName).ToVisibility();
+
+        private static Visibility GetAsVisibilityFromTruthString(XElement r, string attributeName) 
+            => GetAsString(r, attributeName).VisibilityFromTruthString();
+
+        private static int GetAsNumber(XElement r, string attributeName) 
+            => GetAsString(r, attributeName).ToNumber();
     }
 }
